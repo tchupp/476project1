15cea518ef8e283f7be0950aa91606526e78e883
evanhlavaty <hlavaty1@msu.edu>
2015-10-12 18:07:47 -0400

    evan intent that passes data

--- app/src/main/java/edu/msu/chuppthe/steampunked/MainMenuActivity.java
+++ app/src/main/java/edu/msu/chuppthe/steampunked/MainMenuActivity.java
@@ -6,8 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    public static final String GRID_SELECTION = "Grid Selection";
+    public static final String PLAYER_ONE = "Player One";
+    public static final String PLAYER_TWO = "Player Two";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +46,42 @@ public class MainMenuActivity extends AppCompatActivity {
 
     public void onStart(View view) {
         Intent intent = new Intent(this, GameLiveActivity.class);
+
+        if (getFiveRadioButton().isSelected()) {
+            intent.putExtra(GRID_SELECTION, "5");
+        }
+        else if (getTenRadioButton().isSelected())
+        {
+            intent.putExtra(GRID_SELECTION, "10");
+        }
+        else if (getTwentyRadioButton().isSelected())
+        {
+            intent.putExtra(GRID_SELECTION,"20");
+        }
+
+        intent.putExtra(PLAYER_ONE, getPlayerOneTextField().getText());
+        intent.putExtra(PLAYER_TWO, getPlayerTwoTextField().getText());
+
         startActivity(intent);
+    }
+
+    private RadioButton getFiveRadioButton() {
+        return (RadioButton)findViewById(R.id.gridFiveRadio);
+    }
+
+    private RadioButton getTenRadioButton() {
+        return (RadioButton)findViewById(R.id.gridTenRadio);
+    }
+
+    private RadioButton getTwentyRadioButton() {
+        return (RadioButton)findViewById(R.id.gridTwentyRadio);
+    }
+
+    private EditText getPlayerOneTextField() {
+        return (EditText)findViewById(R.id.playerOneTextField);
+    }
+
+    private EditText getPlayerTwoTextField() {
+        return (EditText)findViewById(R.id.playerTwoTextField);
     }
 }
