5a1de93cbf337d62252abecbbbb3855c685c68fb
evanhlavaty <hlavaty1@msu.edu>
2015-10-12 11:20:20 -0400

    evan onStart intent

--- app/src/main/java/edu/msu/chuppthe/steampunked/MainMenuActivity.java
+++ app/src/main/java/edu/msu/chuppthe/steampunked/MainMenuActivity.java
@@ -1,9 +1,11 @@
 package edu.msu.chuppthe.steampunked;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -33,5 +35,10 @@ public class MainMenuActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onStart(View view) {
+        Intent intent = new Intent(this, GameLiveActivity.class);
+        startActivity(intent);
     }
 }
