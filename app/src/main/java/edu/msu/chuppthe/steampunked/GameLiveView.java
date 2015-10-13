e37a8e4ba1a9b8b85839fa4bbb9986a782d478ba
abigaelonchiri <onchiria@msu.edu>
2015-10-13 10:27:30 -0400

    some drawing stuff

--- app/src/main/java/edu/msu/chuppthe/steampunked/GameLiveView.java
+++ app/src/main/java/edu/msu/chuppthe/steampunked/GameLiveView.java
@@ -7,13 +7,24 @@ import android.view.View;
 
 public class GameLiveView extends View {
 
+
+
     /**
+
      * The play area
      */
     private PlayingArea playingArea;
 
+    /**
+     *pipe to draw
+     */
+    private Pipe pipe ;
+
+
+
     public GameLiveView(Context context) {
         super(context);
+
         init(null, 0);
     }
 
@@ -28,22 +39,31 @@ public class GameLiveView extends View {
     }
 
     private void init(AttributeSet attrs, int defStyle) {
+
+
+
         playingArea = new PlayingArea(5, 5);
 
         Pipe player1StartPipe = Pipe.createStartingPipe(getContext());
         Pipe player2StartPipe = Pipe.createStartingPipe(getContext());
         playingArea.add(player1StartPipe, 0, 1);
-        playingArea.add(player2StartPipe, 0, 3);
+        playingArea.add(player2StartPipe, 0, 2);
 
         Pipe player1EndPipe = Pipe.createEndingPipe(getContext());
         Pipe player2EndPipe = Pipe.createEndingPipe(getContext());
         playingArea.add(player1EndPipe, 4, 1);
-        playingArea.add(player2EndPipe, 4, 3);
+        playingArea.add(player2EndPipe, 4, 2);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+
+
+
         playingArea.draw(canvas);
+        pipe.draw(canvas);
     }
+
+
 }
