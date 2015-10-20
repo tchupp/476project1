460edb81f3c03245a1de188e7c8d553149345c74
abigaelonchiri <onchiria@msu.edu>
2015-10-20 00:54:40 -0400

    player class

---
+++ app/src/main/java/edu/msu/chuppthe/steampunked/Player.java
@@ -0,0 +1,15 @@
+package edu.msu.chuppthe.steampunked;
+
+/**
+ * Created by abigaelonchiri on 10/20/15.
+ */
+public class Player {
+
+    private Player Player1;
+    private Player Player2;
+    private Player ActivePlayer;
+
+    public void setActivePlayer(Player activePlayer) {
+        ActivePlayer = activePlayer;
+    }
+}
